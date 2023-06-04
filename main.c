b014523d59109f0724caacaa65293ec6ef66c82a
VICTORIA <2102175@ucc.edu.ar>
2023-06-04 11:20:30 -0300

    subo cambios

---
+++ main.c
@@ -0,0 +1,160 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h> 
+#include <Windows.h>
+
+
+
+void disp_binary (unsigned char dato);
+void retardo(unsigned long int a);
+void choque (unsigned long int speed);
+void auto_fantastico(unsigned long int);
+void carrera (unsigned long int speed);
+
+
+int main() {
+
+    unsigned long int speedini = 150000000;
+    unsigned long int speed = 150000000;
+    speed = speedini;
+
+    int aux = 0;
+    do {    
+        printf("\t -|MENU|- \n");
+            printf("|1|. AUTO FANTASTICO\n");
+            printf("|2|. CHOQUE\n");
+            printf("|3|. LA CARRERA\n");
+            printf("|4| \n");
+            printf("|5|\n");
+            printf("|6|. SALIR\n");
+            printf("Ingrese la opcion:");
+            scanf("%d",&aux);
+        switch (aux) {
+            case 1:
+                printf("\tAUTO FANTASTICO\n\n");
+                auto_fantastico(speed);
+                system("pause");
+                break;
+            case 2:
+                printf("\tCHOQUE\n\n");
+                choque(speed);
+                system("pause");
+                break;
+            case 3:
+                printf("\tLA CARRERA\n\n");
+                carrera(speed);
+                system("pause");        
+                break;
+            case 4:
+                system("pause");        
+                break;
+            case 5:
+                system("pause");        
+            case 6:
+                printf("Hasta la proxima\n");
+                system("pause");
+            default:
+                printf("Opcion no valida, ingrese otro numero\n");
+                system("pause");
+        }
+        system("cls");
+    } while (aux != 6);
+
+
+}
+
+
+void disp_binary (unsigned char dato){
+
+  for (unsigned char mask = 128; mask > 0; mask>>=1)
+    {
+        if (dato&mask)
+        {
+            printf("*");
+        }
+        else {
+            printf("_");
+        }
+    }
+}
+
+void retardo(unsigned long int a){
+    while (a) a--;
+}
+
+void choque (unsigned long int speed){
+  unsigned char tabla[] = {0x81, 0x42, 0x24, 0x18, 0x24, 0x42};
+    while(1){
+
+    for (int i = 0; i < 6; ++i) {
+            printf("\t |EL CHOQUE|\n\n");
+            printf("Presione ESC para volver al menu principal\n\n\n");
+            printf("\tDelay: \t");("speed");
+            disp_binary(tabla[i]);
+            retardo(speed);
+            system("cls");
+
+            if(GetAsyncKeyState(VK_ESCAPE) & 0x0001){return;}
+        }
+    }
+}
+
+void auto_fantastico(unsigned long int speed){
+  unsigned int pos = 0x80;
+  while(1){
+    for (int i = 0; i < 8; ++i) {
+        printf("\t |AUTO FANTASICO|\n\n");
+        printf("Presione ESC para volver al menu principal\n\n");
+        printf("\tDelay: \t");(speed);
+        disp_binary(pos);
+        pos >>= 1;
+        retardo(speed);
+        system("cls");
+
+        if ((speed - 5000000) > 1000000){
+                if(GetAsyncKeyState(VK_UP) & 0x0001){speed += 5000000;}
+            }
+            if(GetAsyncKeyState(VK_DOWN) & 0x0001){speed -= 5000000;}
+            if(GetAsyncKeyState(VK_ESCAPE) & 0x0001){return;}
+
+    }
+    pos = 0x02;
+    for (int i = 0; i < 6; ++i) {
+            printf("\t |Auto Fantastico|\n\n");
+            printf("Presione ESC para volver al menu principal");
+            printf("\tDelay: \t");(speed);
+            disp_binary(pos);
+            pos <<= 1;
+            retardo(speed);
+            system("cls");
+
+
+        if ((speed - 5000000) > 1000000){
+            if(GetAsyncKeyState(VK_UP) & 0x0001){speed += 5000000;}
+            }
+            if(GetAsyncKeyState(VK_DOWN) & 0x0001){speed -= 5000000;}
+            if(GetAsyncKeyState(VK_ESCAPE) & 0x0001){return;}
+        }
+    }
+}
+
+void carrera (unsigned long int speed){
+    unsigned char tabla[] = {0xA0,0x30,0x08,0x06,0x03,0x01};
+    while(1){
+
+        for (int i = 0; i < 6; ++i) {
+            printf("\t |LA CARRERA| \n\n");
+            printf("Presione ESC para volver al menu principal\n\n");
+            printf("\tDelay: \t");(speed); 
+            disp_binary(tabla[i]);
+            retardo(speed);
+            system("cls");
+
+        if ((speed - 5000000) > 1000000){
+            if(GetAsyncKeyState(VK_UP) & 0x0001){speed += 5000000;}
+            }
+            if(GetAsyncKeyState(VK_DOWN) & 0x0001){speed -= 5000000;}
+            if(GetAsyncKeyState(VK_ESCAPE) & 0x0001){return;}
+        }
+    }
+}
